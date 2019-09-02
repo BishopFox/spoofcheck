efecbbf11c590f8c4f805bd3a574349f1ae7551b
Alexandr Savca <alexandr.savca89@gmail.com>
2019-09-02 15:53:37 +0300

    port to python3

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:2.7
+FROM python:3.7
 
 COPY requirements.txt /tmp/requirements.txt
-RUN python -m pip install -r /tmp/requirements.txt
+RUN python3 -m pip install -r /tmp/requirements.txt
 
 WORKDIR /spoofcheck
 COPY . .
--- libs/PrettyOutput.py
+++ libs/PrettyOutput.py
@@ -2,16 +2,16 @@ from colorama import Fore, Back, Style
 from colorama import init as color_init
 
 def output_good(line):
-    print Fore.GREEN + Style.BRIGHT + "[+]" + Style.RESET_ALL, line
+    print(Fore.GREEN + Style.BRIGHT + "[+]" + Style.RESET_ALL, line)
 
 def output_indifferent(line):
-    print Fore.BLUE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line
+    print(Fore.BLUE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line)
 
 def output_error(line):
-    print Fore.RED + Style.BRIGHT + "[-] !!! " + Style.NORMAL, line, Style.BRIGHT + "!!!"
+    print(Fore.RED + Style.BRIGHT + "[-] !!! " + Style.NORMAL, line, Style.BRIGHT + "!!!")
 
 def output_bad(line):
-    print Fore.RED + Style.BRIGHT + "[-]" + Style.RESET_ALL, line
+    print(Fore.RED + Style.BRIGHT + "[-]" + Style.RESET_ALL, line)
 
 def output_info(line):
-    print Fore.WHITE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line
+    print(Fore.WHITE + Style.BRIGHT + "[*]" + Style.RESET_ALL, line)
